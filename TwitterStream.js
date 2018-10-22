e899b03e586d53c6ea49d3ba04f40d6d631e0892
unknown <dumas.clt@gmail.com>
2018-10-22 21:50:11 +0200

    TwitterStream

---
+++ TwitterStream.js
@@ -0,0 +1,14 @@
+const { Readable } = require("stream");
+class TwitterStream extends Readable {
+    constructor(twitterClient) {
+        super({ objectMode: true });
+        this.client = twitterClient;
+    }
+    _read() { }
+    track(query) {
+        this.stream = this.client.stream("statuses/filter", { track: query });
+        this.stream.on("data", tweet => this.push(tweet));
+        this.stream.on("error", err => console.error(err));
+    }
+}
+module.exports = TwitterStream;
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const WebSocket = require("ws");
 const server = http.createServer();
+const Twitter = require("twitter");
+const dotenv = require("dotenv").dotenv.config();
+const TwitterStream = require("./TwitterStream");
+
 const wsServer = new WebSocket.Server({ server });
 server.on("request", (request, response) => {
     const fileSrc = fs.createReadStream("./public/index.html");
@@ -11,3 +15,12 @@ wsServer.on("connection", ws => {
 
 })
 server.listen(8080);
+
+const twitterClient = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+const stream = new TwitterStream(twitterClient);
+stream.track("javascript");
