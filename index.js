f6f9d355c3c76fcf054677020c653ddc883d643f
ClementDums <dumas.clt@gmail.com>
2018-10-22 09:57:11 +0200

    websocket connection

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const fs = require("fs");
 const http = require("http");
 const WebSocket = require("ws");
 const server = http.createServer();
 const wsServer = new WebSocket.Server({ server });
 server.on("request", (request, response) => {
-    // handle requests
-    // handle http requests
+    const fileSrc = fs.createReadStream("./public/index.html");
+    fileSrc.pipe(response);
 })
 wsServer.on("connection", ws => {
-    // handle ws connection
+
 })
 server.listen(8080);
